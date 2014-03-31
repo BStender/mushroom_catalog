3d79c9fea39a68decf520517dd69404381b2a7e7
Kevin Krauss <earlkrauss@gmail.com>
2014-03-30 18:53:09 -0700

    starting the mushroom catalog project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MushroomCatalog::Application.config.secret_key_base = 'b91ac878d7064a035c58e5b10b62a144b2b21c2f8c2c402f1749b2a409d3cd16f9b8e5439384a77503f6fd5d226883a3ae9985336f3cc482f4fd3ea275cf1fd3'
